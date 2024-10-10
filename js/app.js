2b82036b85886a76cf0278647041811a93f60e1f
gimsieun67 <gimsieun67>
2024-10-10 22:34:33 +0200

    test 2

--- js/app.js
+++ js/app.js
@@ -1,20 +1,12 @@
-const videos = document.querySelector(".Puppet_Master_video");
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
+const video = document.getElementById("myVideo");
+const button = document.getElementById("playPauseButton");
 
-let i = 0;
-next.addEventListener("click", () => {
-  if (i < 2) {
-    // 슬라이드가 3개일 때, 마지막 슬라이드로 넘어가지 않도록 제한
-    i++;
-    videos.style.transform = `translateX(-${i * 100}vw)`;
-  }
-});
-
-prev.addEventListener("click", () => {
-  if (i > 0) {
-    // 첫 번째 슬라이드로 돌아가지 않도록 제한
-    i--;
-    videos.style.transform = `translateX(-${i * 100}vw)`; // 슬라이드를 뒤로 이동
+button.addEventListener("click", () => {
+  if (video.paused) {
+    video.play();
+    button.textContent = "Pause"; // 비디오가 재생 중일 때는 'Pause' 버튼
+  } else {
+    video.pause();
+    button.textContent = "Play"; // 비디오가 멈췄을 때는 'Play' 버튼
   }
 });
